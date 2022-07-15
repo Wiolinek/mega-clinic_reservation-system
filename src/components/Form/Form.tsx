315be323bc069d890c33e4d6d52dd71e056dda7c
Wiola P <wiol@onet.eu>
2022-07-15 18:07:41 +0200

    Reservation Form functions fix

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,43 +1,73 @@
 import React, { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import CalendarComp from '../Calendar/Calendar';
-
-import { PacientType } from '../../types/pacient'
+import ButtonLink from '../ButtonLink/ButtonLink'
+import { PacientType } from '../../types/pacient';
+import { DoctorType } from '../../types/doctor';
 
 import '../Form/Form.css';
 
 interface Props {
     specialitiesList: React.ReactNode[];
-    doctorsList?: React.ReactNode[];
+    doctorsData?: DoctorType[];
     timeList?: React.ReactNode[];
 }
 
 
-const Form: React.FC<Props> = ({ specialitiesList, doctorsList, timeList }) => {
+const Form: React.FC<Props> = ({ specialitiesList, doctorsData, timeList }) => {
     const [searchParams, setSearchParams] = useSearchParams();
     const doctorSpec = searchParams.get('speciality');
     const doctorName = searchParams.get('doctor');
     const defaultValue = '---';
-    const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState<any>();
+    const navigate = useNavigate();
 
     const [formData, setFormData] = useState<PacientType>({
         speciality: '',
         doctor: '',
-        date: date.toLocaleDateString('sv'),
+        doctorId: '',
+        date: date?.toLocaleDateString('sv') || '',
         time: '',
         pacientName: '',
         pacientEmail: '',
         pacientPhone: 0,
     });
 
+    const doctorsList = doctorsData?.map(item => 
+        <option key={item.doctor_id}
+            value={item.doctor_id}
+            label={item.name}
+            selected={item.name === doctorName}
+        >
+            {item.name}
+        </option>)
+
     const selectHandler = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setFormData(dataItem => ({...dataItem, [e.target.name]: e.target.value}));
-        setSearchParams(e.target.selectedOptions[0].value !== defaultValue ? {speciality: `${doctorSpec}`, [e.currentTarget.name]: e.target.selectedOptions[0].value} : {});
+        setFormData(dataItem => ({
+            ...dataItem,
+            [e.target.name]: e.target.value,
+            doctorId: e.target.selectedOptions[0].value,
+            time: defaultValue}));
+        setSearchParams(e.target.selectedOptions[0].label !== defaultValue ? {
+            speciality: `${doctorSpec}`,
+            [e.currentTarget.name]: e.target.selectedOptions[0].label
+        } : {}
+        );
     };
 
-    const timeSelectHandler = (e: React.ChangeEvent<HTMLSelectElement>) => setFormData(dataItem => ({...dataItem, [e.target.name]: e.target.value}));
+    const timeSelectHandler = (e: React.ChangeEvent<HTMLSelectElement>) => 
+        setFormData(dataItem => ({
+            ...dataItem,
+            [e.target.name]: e.target.value
+        })
+    );
 
-    const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => setFormData(dataItem => ({...dataItem, [e.target.name]: e.target.value}));
+    const inputHandler = (e: React.ChangeEvent<HTMLInputElement>) => 
+        setFormData(dataItem => ({
+            ...dataItem,
+            [e.target.name]: e.target.value
+        })
+    );
 
     const formHandler = (e: React.SyntheticEvent) => {
         e.preventDefault()
@@ -46,57 +76,87 @@ const Form: React.FC<Props> = ({ specialitiesList, doctorsList, timeList }) => {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({formData})
         })
-        .catch(error => console.log(`error ${error}`))
+        .catch(error => console.log(`error ${error}`));
+        navigate('../success', { replace: true })
     };
 
     useEffect(() => {
-        doctorName && doctorSpec && setFormData({...formData, speciality: doctorSpec, doctor: doctorName, date: date.toLocaleDateString('sv')});
+        const doctor = doctorsData?.filter(doctor => doctor?.name === doctorName && doctor);
+        doctorName && doctorSpec && setFormData({
+            ...formData,
+            speciality: doctorSpec,
+            doctor: doctorName,
+            doctorId: String(doctor?.[0].doctor_id) || '0',
+            date: date?.toLocaleDateString('sv') || ''
+        });
     }, [date, doctorName, doctorSpec]);
     
 
     return (
         <><h2>Zarezerwuj wizytę</h2>
-        <form className='reservation__form' onSubmit={e => formHandler(e)}>
+        <form className='reservation__form' onSubmit={formHandler}>
             <div>
                 <label>Wybierz specjalizację:
-                    <select name='speciality' required onChange={selectHandler}>
-                        <option key={defaultValue} value={defaultValue} >{defaultValue}</option>
+                    <select name='speciality'
+                        onChange={selectHandler}
+                        required>
+                        <option key={defaultValue} value='' >{defaultValue}</option>
                         {specialitiesList}
                     </select>
                     <span>np. pediatra</span>
                 </label>
                 <label>Wybierz lekarza:
-                    <select name='doctor' required onChange={selectHandler}>
-                        <option key={defaultValue} value={defaultValue}>{defaultValue}</option>
+                    <select name='doctor'
+                        onChange={selectHandler}
+                        required>
+                        <option key={defaultValue} value='' label={defaultValue}>{defaultValue}</option>
                         {doctorsList}
                     </select>
                     <span>np. Adam Kowalski</span>
                 </label>
-                <label>Wybierz datę:
-                    <input name='date' value={date.toLocaleDateString()} onChange={e => inputHandler(e)}></input>
+                <label>Wybierz datę z kalendarza:
+                    <input name='date'
+                        value={'' || date?.toLocaleDateString('pl-PL')}
+                        onChange={e => setFormData(dataItem => ({...dataItem, [e.target.name]: date.toLocaleDateString('sv')}))}
+                        required
+                        disabled>
+                    </input>
                     <CalendarComp date={date} setDate={setDate}/>
                 </label>
                 <label>Wybierz godzinę:
-                    <select name='time' required onChange={timeSelectHandler}>
-                        <option key={defaultValue} value={defaultValue} selected>{defaultValue}</option>
+                    <select name='time'
+                        onChange={timeSelectHandler}
+                        required>
+                        <option key={defaultValue} value='' selected>{defaultValue}</option>
                         {timeList}
                     </select>
                     <span>np. 7:30</span>
                 </label>
                 <label>Imię i nazwisko:
-                    <input type='text' name='pacientName' pattern="[a-zA-Z]+[ ][a-zA-Z]+" required onChange={inputHandler}></input>
+                    <input type='text' name='pacientName'
+                        pattern='[a-zA-Z]+[ ][a-zA-Z]+'
+                        onChange={inputHandler}
+                        required>
+                    </input>
                     <span>np. Anna Nowak</span>
                 </label>
                 <label>Adres email:
-                    <input type='email' name='pacientEmail' required onChange={inputHandler}></input>
+                    <input type='email' name='pacientEmail'
+                        onChange={inputHandler}
+                        required>
+                    </input>
                     <span>np. anna.nowak@email.pl</span>
                 </label>
                 <label>Numer telefonu:
-                    <input type='tel' name='pacientPhone' pattern="[0-9]{9}" required onChange={inputHandler}></input>
+                    <input type='tel' name='pacientPhone'
+                        pattern='[0-9]{9}'
+                        onChange={inputHandler}
+                        required>
+                    </input>
                     <span>np. 123456789</span>
                 </label>
             </div>
-            <button type="submit">Wyślij</button>
+            <ButtonLink type='submit' customClass='btn reservation__form-btn' text='Wyślij'/>
         </form>
     </>
     )
