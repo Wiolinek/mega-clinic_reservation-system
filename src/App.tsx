01219aa3da59053d1ce82fede3fc49fba7e90f5d
Wiola P <wiol@onet.eu>
2022-07-15 17:50:09 +0200

    add Logout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-// import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navigation from './components/Navigation/Navigation'
 import Footer from './components/Footer/Footer'
 import Homepage from './components/Homepage/Homepage'
-import AboutUs from './components/AboutUs/AboutUs'
 import OurDoctors from './components/OurDoctors/OurDoctors'
-import Contact from './components/Contact/Contact'
 import Reservation from './components/Reservation/Reservation'
 import LogIn from './components/LogIn/LogIn'
+import SuccessForm from './components/SuccessForm/SuccessForm'
 import DoctorAccount from './components/DoctorAccount/DoctorAccount'
+import Logout from './components/Logout/Logout'
+import ErrorPage from './components/ErrorPage/ErrorPage'
 import Context from './Context'
 
 import './App.css';
@@ -21,12 +21,13 @@ const App: React.FC = () => {
   <Navigation />
     <Routes>
       <Route path='/' element={ <Homepage />}></Route>
-      <Route path='/about-us' element={ <AboutUs />}></Route>
       <Route path='/our-doctors' element={ <OurDoctors />}></Route>
-      <Route path='/contact' element={ <Contact />}></Route>
       <Route path='/reservation' element={ <Reservation />}></Route>
       <Route path='/login' element={ <LogIn />}></Route>
-      <Route path='/user' element={ <DoctorAccount />}></Route>
+      <Route path='/success' element={ <SuccessForm />}></Route>
+      <Route path='/doctor-account' element={ <DoctorAccount />}></Route>
+      <Route path='/logout' element={ <Logout />}></Route>
+      <Route path='*' element={ <ErrorPage />}></Route>
     </Routes>
     <Footer />
     </Context>
---
+++ src/components/Logout/Logout.tsx
@@ -0,0 +1,40 @@
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import GenericSection from '../GenericSection/GenericSection';
+import { myContext } from '../../Context';
+
+
+const Logout: React.FC = () => {
+    const context = useContext(myContext)
+    const navigate = useNavigate();
+
+    const content: React.ReactNode = <>
+        <h2>Trwa wylogowywanie.</h2>
+        <p>Za chwilę zostaniesz przekierowany na stronę główną.</p>
+    </>
+
+    useEffect(() => {
+        fetch(`http://localhost:3030/logout`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+        })
+        .catch(error => console.log(`error ${error}`))
+    },[])
+
+    useEffect(() => {
+        context?.setUser?.(false)
+        setTimeout(() => {
+            navigate('../', { replace: true })
+        }, 2000)
+    },[])
+
+
+    return (
+        <main>
+            <GenericSection children={content} customClass='login__section' />
+        </main>
+    )
+}
+
+export default Logout;
